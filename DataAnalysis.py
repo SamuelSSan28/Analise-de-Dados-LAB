e395c6ca1a7b20ad22312b20ffcb6f365a748661
Samuel <samuelssan28@gmail.com>
2020-01-09 17:20:18 -0300

    Adicionando mais Funcoes

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import calendar
+import statistics
 from Graphics import GraficosClass as graph
 from datetime import datetime
 from datetime import timedelta
@@ -27,23 +28,91 @@ class DataAnalysisClass:
         graph.GeraPizza(fractions,offsets,labels)
 
 
-    def bancadaFrio(self):
-        pass
+    def bancadaFrio(self,df):
+        fractions = df.loc[df["Feedback da Temperatura"] == "Frio"]["Informe a sua bancada"].value_counts()
+        labels = 'B1', 'B2', 'B3', 'B4','B5','B6','B7','M'
+        offsets = (0.1, 0.05, 0.1, 0.08)
+        graph.GeraPizza(fractions, offsets, labels)
 
-    def bancadaCalor(self):
-        pass
 
-    def sensasacaoM(self):
-        pass
+    def bancadaCalor(self,df):
+        fractions = df.loc[df["Feedback da Temperatura"] == "Quente"]["Informe a sua bancada"].value_counts()
+        labels = 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'M'
+        offsets = (0.1, 0.05, 0.1, 0.08)
+        graph.GeraPizza(fractions, offsets, labels)
 
-    def sensasacaoT(self):
-        pass
 
-    def sensasacaoN(self):
-        pass
+    def sensasacaoM(self,df):
+        labels = 'Frio', 'Agradavel', 'Quente', 'Muito Frio'
+        fractions = df.query("Hora > '08:00:00' and Hora < '12:00:00'")["Feedback da Temperatura"].value_counts()
+        offsets = (0.1, 0.05, 0.1, 0.08)
+        graph.GeraPizza(fractions, offsets, labels)
 
-    def temperaturaMedia_Dia(self):
-        pass
+    def sensasacaoT(self,df):
+        labels = 'Frio', 'Agradavel', 'Quente', 'Muito Frio'
+        fractions = df.query("Hora > '12:00:00' and Hora < '18:00:00'")["Feedback da Temperatura"].value_counts()
+        offsets = (0.1, 0.05, 0.1, 0.08)
+        graph.GeraPizza(fractions, offsets, labels)
+
+
+    def sensasacaoN(self,df):
+        labels = 'Frio', 'Agradavel', 'Quente', 'Muito Frio'
+        fractions = df.query("Hora > '18:00:00' and Hora < '20:00:00'")["Feedback da Temperatura"].value_counts()
+        offsets = (0.1, 0.05, 0.1, 0.08)
+        graph.GeraPizza(fractions, offsets, labels)
+
+    def temperaturaMedia_Dia(self,df_sensores):
+        data_temp = {'Data': [],
+                     'Temperatura_Max': [],
+                     'Temperatura_Min': [],
+                     'Temperatura_Med': []
+                     }
+
+        for i in range(1, 13):
+            quant_dias_mes = calendar.monthrange(2019, i)
+            for j in range(1, quant_dias_mes[1] + 1):
+                d = j
+                m = i
+                dia = "2019"
+
+                if m < 10:
+                    dia = "0" + str(m) + "/" + dia
+                else:
+                    dia = str(m) + "/" + dia
+
+                if d < 10:
+                    dia = "0" + str(d) + "/" + dia
+                else:
+                    dia = str(d) + "/" + dia
+
+                cmd = "dia == '" + dia + "'"
+                result = df_sensores.query(cmd)
+                temp_dia = []
+                temp_min = []
+
+                for j in result["temperatura"]:
+                    if len(temp_min) < 5:
+                        temp_min.append(j)
+
+                    if len(temp_min) == 4:
+                        med = sum(temp_min) / len(temp_min)
+                        temp_dia.append(med)
+                        temp_min = []
+
+                result["temperatura"].replace(temp_dia)
+                if len(result) > 0:
+                    max_temp_dia = result["temperatura"].max()
+                    min_temp_dia = result["temperatura"].min()
+                    med_temp_dia = result["temperatura"].mean()
+
+                    data_temp['Data'].append(dia[0:2] + "/10/2019")#ver isso aqui
+                    data_temp['Temperatura_Max'].append(round(max_temp_dia))
+                    data_temp['Temperatura_Min'].append(round(min_temp_dia))
+                    data_temp['Temperatura_Med'].append(round(med_temp_dia))
+
+        dados = pd.DataFrame(data_temp)
+        graph.geraHistoGrama(dados[['Data', 'Temperatura_Max', 'Temperatura_Med','Temperatura_Min']],'Data')
+
 
     def kiloWatt_hora(self,df):
         corrente_por_dia = df[['corrente', 'horario']]
